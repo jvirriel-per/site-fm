3fdd2ba1b4bd761a05628c39247b5c458ba14553
marcoshernandezvs <47675134+marcoshernandezvs@users.noreply.github.com>
2019-03-01 13:19:21 -0400

    improve mobile menu and responsive function

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 import Logo from '../assets/logo_blue.svg';
+import Points from '../assets/active.svg';
 
 
 class Navigation extends Component {
+  state = {
+    about: false,
+    product: false,
+    pricing: false
+   }
 
+    addImage = (id) => {
+    var element = document.getElementById(id);
+  }
     render() {
     function responsive() {
       let x = document.getElementById("myTopnav");
@@ -17,20 +26,24 @@ class Navigation extends Component {
 
         return (
           
-            <div className="topnav" >
-              <nav className="d-flex align-item-center contenedor m-auto" id="myTopnav">
-                <img className="mr-auto ml-xs-2" src={Logo} alt="FeatureMine"/>
+            <div className="topnav d-flex align-item-center" id="myTopnav">
+           
+                <img className="mr-auto" src={Logo} alt="FeatureMine"/>
                 
-                  <a href="#" className="hov">ABOUT</a>
-                  <a href="#"className="hov">PRODUCT</a>
-                  <a href="#"className="hov mr-4">PRICING</a>
-                  <a href="#"className="hov mr-2"><strong>LOGIN</strong></a>
-                  <a href="#" className="active hov mr-md-2 mr-lg-2">OPEN ACCOUNT</a>
+                <a href="#" className="hov" id="about" onClick={() => { this.setState({ about: true, product: false, pricing: false})}}>ABOUT { this.state.about && <img src={Points} />}</a>
+                
+                <a href="#" className="hov" id="product" onClick={() => { this.setState({ product: true, about: false, pricing: false})}}>PRODUCT { this.state.product && <img src={Points} />}</a>
+                
+                <a href="#" className="hov" id="pricing" onClick={() => { this.setState({ pricing: true, about: false, product:false})}}>PRICING { this.state.pricing && <img src={Points} />}</a>
+                
+                <a href="#"className="hov mr-2" id="login"><strong>LOGIN</strong></a>
+                <a href="#" className="active hov" id="account">
+                      OPEN ACCOUNT
+                  </a>
                   <a href="javascript:void(0);" className="icon" onClick={() => responsive()}>
                       <i className="fa fa-bars" />
                   </a>
                 
-              </nav>  
             </div>
           
         );
