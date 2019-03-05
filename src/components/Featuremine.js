c0ac470f8e684c5239e8b460cde0880b75401cf8
marcoshernandezvs <47675134+marcoshernandezvs@users.noreply.github.com>
2019-03-04 20:31:54 -0400

    new responsive design adjust

--- src/components/Featuremine.js
+++ src/components/Featuremine.js
@@ -4,7 +4,7 @@ import { Container, Row, Col } from 'reactstrap';
 const Featuremine = () => (
   <Container fluid className="principal align-items-center justify-content-center">
     <Row className="contenedor">
-      <Col xs="12 order-1" md="12 order-md-1"  lg="5 order-lg-0">
+      <Col xs="12 order-1" md="12 order-md-1"  lg="5 order-lg-0" className="d-flex align-item-center">
       <div className="__featuremine-first-box">
               <p className="title"><strong>FEATUREMINE</strong></p>
               <p className="second-title"><strong>Power to get
@@ -15,14 +15,14 @@ const Featuremine = () => (
               and more successful. Here are the features and
               components of the worlds most advanced quant
               ecosystem.</p>
-              <Row className="align-item-center justify-content-between mr-1 ml-1 call-t-action">
+              <div className="align-item-center justify-content-between mr-1 ml-1 call-t-action">
                 
                   <button className="black-button"><strong>TRY IT NOW</strong></button>
                 
                 
                   <button className="white-button"><strong>LEARN MORE <i className="fas fa-arrow-right"></i></strong></button>
                 
-              </Row>
+              </div>
             
           </div>
       </Col>
